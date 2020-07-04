e64ade36e4200fc40c39f005cdce78f753279433
MaxxRider2 <66148227+MaxxRider2@users.noreply.github.com>
2020-07-04 08:32:34 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -162,7 +162,7 @@ async def upload_to_gdrive(file_upload, message, messa_ge, g_id):
             button.append([pyrogram.InlineKeyboardButton(text="👉DIRECT LINK 😁", url=f"{tam_link}")])
         button_markup = pyrogram.InlineKeyboardMarkup(button)
         await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
-        await messa_ge.reply_text(f"🤖: {file_upload} <b>has been Uploaded successfully to your Cloud😌</b> <i><b>💥Join Our TEAM DRIVE to Access Link😊You can't Access Link without Join Team Drive👉 /JOIN_TEAM_DRIVE</b></i>  |<a href='tg://user?id={g_id}'> </a>", reply_markup=button_markup),
+        await messa_ge.reply_text(f"🤖: {file_upload} <b>has been Uploaded successfully to your Cloud😌</b> <i><b>💥Join Our TEAM DRIVE to Access Link😊You can't Access Link without <a href="https://t.me/MaxxRider"> MaxxRider </a>  |<a href='tg://user?id={g_id}'> </a>", reply_markup=button_markup),
         os.remove(file_upload)
         await del_it.delete()
     else:
